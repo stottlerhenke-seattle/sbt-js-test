737e44a25ecfa1e5977ae045e0fcf91c25bc2c5c
Joe Barnes <barnesjd@gmail.com>
2016-03-05 14:21:23 -0600

    Adding a test for lsJs

---
+++ src/it/scala/com/joescii/sbtjs/LsJs.scala
@@ -0,0 +1,25 @@
+package com.joescii.sbtjs
+
+class LsJs extends SbtJsTestSpec("lsJs") {
+  "The lsJs project" should {
+    "successfully run 'lsJs'" in {
+      result = runSbt("lsJs")
+      result.futureValue._1 shouldEqual 0
+    }
+  }
+
+  val slash = System.getProperty("file.separator")
+  val slashRegex = java.util.regex.Pattern.quote(slash)
+  def path(line:String):List[String] =
+    line.split(slashRegex).toList.reverse.span(_ != "js")._1.reverse
+
+  "list all 3 js source files" in {
+    whenReady(result) { case (status, out) =>
+      val pathsSorted = out.take(3).map(path).sortBy(_.mkString(slash))
+
+      pathsSorted(0) shouldEqual List("3rdLib", "angular-mocks.js")
+      pathsSorted(1) shouldEqual List("3rdLib", "angular.js")
+      pathsSorted(2) shouldEqual List("Hello.js")
+    }
+  }
+}
