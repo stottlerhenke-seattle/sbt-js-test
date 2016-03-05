39159a3d3e8d088d0cc2a5e7d482b5d8b4d873cc
Joe Barnes <barnesjd@gmail.com>
2016-03-05 17:55:55 -0600

    Correcting AllPass integration test

--- src/it/scala/com/joescii/sbtjs/AllPass.scala
+++ src/it/scala/com/joescii/sbtjs/AllPass.scala
@@ -7,12 +7,17 @@ class AllPass extends SbtJsTestSpec("all-pass") {
       result.futureValue._1 shouldEqual 0
     }
 
+    "cause writeConsoleHtml to run" in {
+      result.futureValue._2(0) should startWith ("[info] Generating")
+      result.futureValue._2(0) should endWith   ("console.html...")
+    }
+
     "announce that the tests are running" in {
-      result.futureValue._2(0) shouldEqual "[info] Running JavaScript tests..."
+      result.futureValue._2(1) shouldEqual "[info] Running JavaScript tests..."
     }
 
     "announce that the task was successful" in {
-      result.futureValue._2(1) should startWith ("[success]")
+      result.futureValue._2.last should startWith ("[success]")
     }
   }
 }
