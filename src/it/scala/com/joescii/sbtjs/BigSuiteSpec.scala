5005264350e7dbf9c3202ca91f9aff5adb750713
Joe Barnes <barnesjd@gmail.com>
2016-03-21 10:36:34 -0500

    Un-ignoring tests that work with workarounds

--- src/it/scala/com/joescii/sbtjs/BigSuiteSpec.scala
+++ src/it/scala/com/joescii/sbtjs/BigSuiteSpec.scala
@@ -2,16 +2,16 @@ package com.joescii.sbtjs
 
 class BigSuiteSpec extends SbtJsTestSpec("bigSuite") {
   "The bigSuite project" should {
-    "successfully run 'jsTest'" ignore {
+    "successfully run 'jsTest'" in {
       result = runSbt("jsTest")
       result.futureValue._1 shouldEqual 0
     }
 
-    "announce that 88 specs ran with 0 failures" ignore {
-      result.futureValue._2.reverse.apply(2) shouldEqual "[info] 88 specs, 0 failures, 4 pending specs"
+    "announce that 88 specs ran with 0 failures" in {
+      result.futureValue._2.reverse.apply(1) shouldEqual "[info] 88 specs, 0 failures, 4 pending specs"
     }
 
-    "announce that the task was successful" ignore {
+    "announce that the task was successful" in {
       result.futureValue._2.last should startWith ("[success]")
     }
   }
